b05ae769903419e3e67f68a87063d9dc15d4ccb3
Finn Masurat <finn.masurat@hotmail.de>
2015-06-02 10:23:13 +0200

    Prim mit Queue als Prim Eigene Alternative als PrimAlt

--- src/de/haw/informatik/algorithms/PrimAlt.java
+++ src/de/haw/informatik/algorithms/PrimAlt.java
@@ -7,66 +7,69 @@ import org.jgrapht.graph.WeightedPseudograph;
 
 import java.util.*;
 
-public class Prim {
+public class PrimAlt {
 
     // Map with all Vertices and the shortest Edge from the spanningTree to them
     public static Map<EFVertex, Double> map = new HashMap<>();
 
     public static Graph computeGraph(Graph graph, EFVertex startVertex) {
 
-
         PriorityQueue<EFVertex> leQueue = new PriorityQueue<>((o1, o2) -> {
             return Double.compare(map.get(o1), map.get(o2));
         });
 
-        // put all Vertices with POSITIV_INFINITY in the map, to check that it will definitly be change
-        for (EFVertex ef : (Set<EFVertex>) graph.vertexSet()) {
-            map.put(ef, Double.POSITIVE_INFINITY);
-            leQueue.add(ef);
-
-        }
 
         // spanningTree-Graph which we will return
         WeightedPseudograph<EFVertex, EFWeightedEdge> spanningTree =
                 new WeightedPseudograph<>(EFWeightedEdge.class);
 
-        // setting startVertex on top of the PriorityQueue
-        spanningTree.addVertex(startVertex);
-        leQueue.remove(startVertex);
-        map.remove(startVertex);
-        map.put(startVertex, 0.0);
-        leQueue.add(startVertex);
 
+        // put all Vertices with POSITIV_INFINITY in the map, to check that it will definitly be change
+        for (EFVertex ef : (Set<EFVertex>) graph.vertexSet()) {
+            map.put(ef, Double.POSITIVE_INFINITY);
+        }
+
+
+        leQueue.add(startVertex);
+        spanningTree.addVertex(startVertex);
 
         while (!leQueue.isEmpty()) {
-
             EFVertex currentVertex = leQueue.poll();
             Set<EFWeightedEdge> set = graph.edgesOf(currentVertex);
 
+            // refreshing the distances from the spanningTree to the Vertices
             for (EFWeightedEdge e : set) {
 
-                // refreshing the distances from the spanningTree to the Vertices
                 if (graph.getEdgeWeight(e) < map.get(graph.getEdgeTarget(e))) {
-                    leQueue.remove(graph.getEdgeTarget(e));
                     EFVertex temp = (EFVertex) graph.getEdgeSource(e);
                     ((EFVertex) graph.getEdgeTarget(e))._predecessor = temp;
                     map.remove(graph.getEdgeTarget(e));
                     map.put((EFVertex) graph.getEdgeTarget(e), graph.getEdgeWeight(e));
-                    leQueue.add((EFVertex) graph.getEdgeTarget(e));
                 }
-                }
+            }
 
-            // add the clostest direct neighbour to the spanningTree
-            // if the spanningTree doesnt contains it already
-            if (!spanningTree.containsVertex(currentVertex)) {
-                spanningTree.addVertex(currentVertex);
-                EFWeightedEdge temporaryEdge = spanningTree.addEdge(currentVertex._predecessor, currentVertex);
-                spanningTree.setEdgeWeight(temporaryEdge, map.get(currentVertex));
+            // check which Vertex is the closest, direct neighbour to the spanningTree
+            Map.Entry<EFVertex, Double> min = null;
+            for (Map.Entry<EFVertex, Double> entry : map.entrySet()) {
+                if (!spanningTree.containsVertex(entry.getKey()) && (min == null || entry.getValue() < min.getValue())) {
+
+                    min = entry;
+                }
+            }
+
+            // add this Vertex to the spanningTree with the edge to it
+            if (min != null) {
+                EFVertex target = min.getKey();
+                spanningTree.addVertex(target);
+                leQueue.add(target);
+                EFWeightedEdge temporaryEdge = spanningTree.addEdge(target._predecessor, target);
+                spanningTree.setEdgeWeight(temporaryEdge, min.getValue());
+
             }
         }
+
         return spanningTree;
     }
 
 
 }
-
