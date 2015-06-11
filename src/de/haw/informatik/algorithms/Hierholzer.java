86c77aac7aebf73ef2e6058dab53d4f1d0386130
Finn Masurat <finn.masurat@hotmail.de>
2015-06-11 23:38:32 +0200

    Hierholzer muss eingebunden werden, dann wird geprüüft

--- src/de/haw/informatik/algorithms/Hierholzer.java
+++ src/de/haw/informatik/algorithms/Hierholzer.java
@@ -12,7 +12,10 @@ import java.util.*;
 public class Hierholzer {
 
 
-    public static String computeEuler(Graph graph, EFVertex source) {
+    public static List<EFVertex> getPath(Graph graph) {
+
+        EFVertex start;
+        EFVertex end;
 
         ArrayList<EFVertex> oddCounter = new ArrayList<>();
         Map<EFVertex, Double> map = new HashMap<>();
@@ -32,8 +35,64 @@ public class Hierholzer {
             throw new IllegalArgumentException("More than two odd vertices");
         }
 
+        if (oddCounter.size() == 2) {
+            start = oddCounter.get(0);
+            end = oddCounter.get(1);
+        } else {
+            start = (EFVertex) graph.vertexSet().iterator().next();
+            end = start;
+        }
 
-        return "";
+        ArrayList<EFVertex> way = new ArrayList<>();
+        int insertIndex = 0;
+        while (graph.edgeSet().size() > 0) {
+
+            List<EFVertex> w = findWay(graph, start, end);
+            way.addAll(insertIndex, w);
+
+            for (int i = 0; i < way.size(); i++) {
+                EFVertex v = way.get(i);
+                if (graph.containsVertex(v)) {
+                    start = v;
+                    end = v;
+                    insertIndex = i;
+                    break;
+                }
+            }
+
+        }
+        return way;
+    }
+
+
+    private static List<EFVertex> findWay(Graph graph, EFVertex start, EFVertex end) {
+        ArrayList<EFVertex> way = new ArrayList<>();
+        EFVertex currentVertex = start;
+
+        do {
+            way.add(currentVertex);
+            ArrayList<EFVertex> neighbours = new ArrayList<>();
+            Set<EFVertex> set = graph.edgesOf(currentVertex);
+            for (EFVertex ef : set) {
+
+                if (graph.getEdgeSource(ef) == currentVertex) {
+                    neighbours.add((EFVertex) graph.getEdgeTarget(ef));
+                } else {
+                    neighbours.add((EFVertex) graph.getEdgeSource(ef));
+                }
+
+            }
+
+            EFVertex next = neighbours.iterator().next();
+            graph.removeEdge(currentVertex, next);
+        } while (!currentVertex.equals(end));
+
+        if (start.equals(end)) {
+            way.add(end);
+        }
+
+        return way;
+
     }
 
 }
